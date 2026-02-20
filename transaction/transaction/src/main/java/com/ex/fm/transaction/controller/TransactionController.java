3d22459f02d8b8974a355f387483442324d13298
Anurath <anurathwaghmode2018@gmail.com>
2026-02-21 01:41:17 +0530

    TSN-02: Create transaction api implemented
    TSN-02: Create transaction api implemented

--- transaction/transaction/src/main/java/com/ex/fm/transaction/controller/TransactionController.java
+++ transaction/transaction/src/main/java/com/ex/fm/transaction/controller/TransactionController.java
@@ -2,6 +2,9 @@ package com.ex.fm.transaction.controller;
 
 import com.ex.fm.transaction.dto.TransactionDTO;
 import com.ex.fm.transaction.entity.Transaction;
+import com.ex.fm.transaction.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/transaction")
 public class TransactionController {
 
+    @Autowired
+    private TransactionService transactionService;
+
     @PostMapping
     public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO request){
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.createTransaction(request));
     }
 }
--- transaction/transaction/src/main/java/com/ex/fm/transaction/service/TransactionService.java
+++ transaction/transaction/src/main/java/com/ex/fm/transaction/service/TransactionService.java
@@ -1,23 +1,36 @@
 package com.ex.fm.transaction.service;
 
 import com.ex.fm.transaction.dto.TransactionDTO;
+import com.ex.fm.transaction.dtotransformer.DtoTransformer;
 import com.ex.fm.transaction.entity.Transaction;
 import com.ex.fm.transaction.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionService {
 
+    public static final Logger LOG = LoggerFactory.getLogger(TransactionService.class);
+
     @Autowired
     private TransactionRepository transactionRepository;
 
-    public Transaction createTransaction(TransactionDTO request){
-        if(request != null){
+    @Autowired
+    private  DtoTransformer dtoTransformer;
 
-            transactionRepository.save(null);
+    public Transaction createTransaction(TransactionDTO request){
+
+        Transaction transaction = new Transaction();
+
+        if(request != null){
+            LOG.info("Transaction proceed to save.");
+            transaction = dtoTransformer.dtoToTransaction(request);
+            transactionRepository.save(transaction);
         }
-        return null;
+
+        return transaction;
     }
 
 }
