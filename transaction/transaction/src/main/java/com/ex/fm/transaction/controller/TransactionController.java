499a9921d2e5a79d46e29e6385b0ccf8d2577ffc
Anurath <131398158+Anurath@users.noreply.github.com>
2026-02-24 05:25:37 +0530

    TSNP-03: API modified from single Transaction operation to List of operation (#8) (#9)
    TSNP-03: API modified from single Transaction operation to List of operation

--- transaction/transaction/src/main/java/com/ex/fm/transaction/controller/TransactionController.java
+++ transaction/transaction/src/main/java/com/ex/fm/transaction/controller/TransactionController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/transaction")
 @CrossOrigin(origins = "http://localhost:5173")
