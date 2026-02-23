d9f33e73765f400d95ba3158a87236b7e5159529
Anurath <131398158+Anurath@users.noreply.github.com>
2026-02-24 05:26:09 +0530

    Feature/tsnp 03 (#10)
    * TSNP-03: API modified from single Transaction operation to List of operation
    
    TSNP-03: API modified from single Transaction operation to List of operation
    
    * TSNP-03: Front end basic react project structure added.
    
    TSNP-03: Front end basic react project structure added.
    
    * TSNP-03: Implement initial code base for Front End
    
    TSNP-03: Implement initial code base for Front End
    
    * TSNP-03: API modified from single Transaction operation to List of operation (#8) (#9)
    
    TSNP-03: API modified from single Transaction operation to List of operation

---
+++ TransactionProcessor/src/App.jsx
@@ -0,0 +1,16 @@
+import Example from "./component/example"
+import ListTransaction from "./component/ListTransaction"
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>Anurath</div>
+      <ListTransaction />
+      <Example />
+    </>
+  )
+}
+
+export default App
--- transaction/transaction/src/main/java/com/ex/fm/transaction/controller/TransactionController.java
+++ transaction/transaction/src/main/java/com/ex/fm/transaction/controller/TransactionController.java
@@ -6,15 +6,17 @@ import com.ex.fm.transaction.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import java.util.List;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/transaction")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TransactionController {
 
     @Autowired
@@ -23,5 +25,10 @@ public class TransactionController {
     @PostMapping
     public ResponseEntity<List<Transaction>> createTransaction(@RequestBody List<TransactionDTO> request){
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.createTransaction(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getListTransactionData(){
+       return ResponseEntity.ok(transactionService.getListTransactionData());
     }
 }
