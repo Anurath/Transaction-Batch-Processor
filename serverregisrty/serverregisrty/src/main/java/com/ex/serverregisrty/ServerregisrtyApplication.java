4461ed6d31ddaf2fa9c2a22fda829b1bea90fd98
Anurath <anurathwaghmode2018@gmail.com>
2026-02-25 01:59:40 +0530

    TSNP-04: New service added for achieving microservices architecture
    TSNP-04: New service added for achieving microservices architecture

---
+++ serverregisrty/serverregisrty/src/main/java/com/ex/serverregisrty/ServerregisrtyApplication.java
@@ -0,0 +1,13 @@
+package com.ex.serverregisrty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerregisrtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerregisrtyApplication.class, args);
+	}
+
+}
---
+++ serverregisrty/serverregisrty/src/test/java/com/ex/serverregisrty/ServerregisrtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.serverregisrty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerregisrtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
