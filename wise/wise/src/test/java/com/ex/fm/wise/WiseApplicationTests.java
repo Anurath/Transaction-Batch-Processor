5077adfcfc32177b7b0c1971219543f435dcac3c
Anurath <131398158+Anurath@users.noreply.github.com>
2026-03-05 00:53:03 +0530

    Feature/tsnp 06 (#17)
    * TSNP-06: Code refactored
    
    TSNP-06: Code refactored
    
    * TSNP-06: File modified
    
    TSNP-06: File modified
    
    * TSNP-06: Extra validation added for amount check.
    
    TSNP-06: Extra validation added for amount check.
    
    * TSNP-6: New service added for AI integration.
    
    TSNP-6: New service added for AI integration.

---
+++ wise/wise/src/test/java/com/ex/fm/wise/WiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.fm.wise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
