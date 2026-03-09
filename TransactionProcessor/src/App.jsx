97759f8a296c27a38e2e814440cd755f14796151
Anurath <anurathwaghmode2018@gmail.com>
2026-03-10 04:26:04 +0530

    WS-10: Implemented Frontend for AI
    WS-10: Implemented Frontend for AI

--- TransactionProcessor/src/App.jsx
+++ TransactionProcessor/src/App.jsx
@@ -1,15 +1,12 @@
-import Example from "./component/example"
+
 import ListTransaction from "./component/ListTransaction"
-import SharvamAudioPlayer from "./component/SharvamAudioPlayer"
 
 function App() {
  
 
   return (
     <>
-      <div>Anurath</div>
       <ListTransaction />
-      <SharvamAudioPlayer />
     </>
   )
 }
